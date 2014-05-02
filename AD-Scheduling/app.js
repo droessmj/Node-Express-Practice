9b899d77716656bd4859377b466debca75a80411
droessmj <droessmj@gmail.com>
2014-05-02 12:44:57 -0500

    wtf

--- AD-Scheduling/app.js
+++ AD-Scheduling/app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongo = require('mongoskin');
+var db = mongo.db('mongodb://localhost:27017/AD-Scheduling', {native_parser: true});
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -23,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/events', events);
+app.use('/refs', refs);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
